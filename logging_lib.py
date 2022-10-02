f1f1ba93296dce18526b0f30bdfef23e4128ea87
Dhaval Savalia <hello@dhavalsavalia.com>
2022-10-02 11:51:52 -0400

    final

--- logging_lib.py
+++ logging_lib.py
@@ -1,6 +1,8 @@
 import logging.config
 import sys
+import time
 import logging
+import json
 from typing import Callable, Optional
 from fastapi import FastAPI, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -59,18 +61,111 @@ class RouterLoggingMiddleware(BaseHTTPMiddleware):
                        request: Request,
                        call_next: Callable
     ) -> Response:
-        response: Response = await call_next(request)
+        response, response_dict = await self._log_response(
+            call_next,
+            request
+        )
 
         logging_dict = dict()
-        logging_dict["request"] = {
-            "method": request.method,
-            "path": request.url.path,
-            "ip": request.client.host
-        }
-        logging_dict["response"] = {
-            "status": "successful" if response.status_code < 400 else "failed",
-            "status_code": response.status_code,
-        }
+        logging_dict["request"] = await self._log_request(request)
+        logging_dict["response"] = response_dict
         self._logger.info(logging_dict)
 
         return response
+    
+    async def _log_request(
+            self,
+            request: Request
+    ) -> str:
+        """Logs request part
+            Arguments:
+           - request: Request
+
+        """
+
+        print("Laaaaaaa")
+
+        path = request.url.path
+        if request.query_params:
+            path += f"?{request.query_params}"
+
+        request_logging = {
+            "method": request.method,
+            "path": path,
+            "ip": request.client.host
+        }
+
+        try:
+            body = await request.json()
+            request_logging ["body"] = body
+        except:
+            body = None
+
+        return request_logging
+    
+    async def _log_response(
+        self,
+        call_next: Callable,
+        request: Request
+    ) -> Response:
+        """Logs response part
+        Arguments:
+            - call_next: Callable (To execute the actual path function and get response back)
+            - request: Request
+        Returns:
+            - response: Response
+            - response_logging: str
+        """
+
+        start_time = time.perf_counter()
+        response = await self._execute_request(call_next, request)
+        finish_time = time.perf_counter()
+
+        overall_status = "successful" if response.status_code < 400 else "failed"
+        execution_time = finish_time - start_time
+
+        response_logging = {
+            "status": overall_status,
+            "status_code": response.status_code,
+            "time_taken": f"{execution_time:0.4f}s"
+        }
+
+        resp_body = [section async for section in response.__dict__["body_iterator"]]
+
+        try:
+            resp_body = json.loads(resp_body[0].decode())
+        except:
+            resp_body = str(resp_body)
+
+        response_logging["body"] = resp_body
+
+        return response, response_logging
+
+    async def _execute_request(self,
+                               call_next: Callable,
+                               request: Request
+    ) -> Response:
+        """Executes the actual path function using call_next.
+
+               Arguments:
+               - call_next: Callable (To execute the actual path function
+                            and get response back)
+               - request: Request
+               Returns:
+               - response: Response
+        """
+        try:
+            response: Response = await call_next(request)
+
+            return response
+
+        except Exception as e:
+            self._logger.exception(
+                {
+                    "path": request.url.path,
+                    "method": request.method,
+                    "reason": e
+                }
+            )
+
+
